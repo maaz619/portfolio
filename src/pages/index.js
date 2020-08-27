256a1e25e0fc2c3435a070527fff7db2e6253b3a
maaz619 <ahmedmaaz919@gmail.com>
2020-08-27 14:28:55 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "../components/Header"
 import styled from "styled-components"
 import Content from "../components/Content"
+import { Helmet } from "react-helmet"
 
 const Mainapp = styled.div`
   height: 100vh;
@@ -10,6 +11,11 @@ const Mainapp = styled.div`
 const IndexPage = () => {
   return (
     <Mainapp>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Portfolio</title>
+        <link rel="icon"></link>
+      </Helmet>
       <div style={{ height: "30px" }}></div>
       <Header />
       <Content></Content>
